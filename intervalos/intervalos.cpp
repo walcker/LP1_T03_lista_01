db1a79b55a5c2d880fca23c366e42a6bbffaab99
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-06-15 08:00:10 -0300

    Update intervalos.cpp

--- intervalos/intervalos.cpp
+++ intervalos/intervalos.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string>
+#include <istream>
+#include <sstream>
+#include <iomanip>
 
+using namespace std;
+
+// Se desejar, crie funções aqui, antes do main().
+
+int main(void)
+{
+
+    int x = 0;
+    int a = 0, b = 0, c = 0, d = 0, e = 0;
+    float A, B, C, D, E;
+
+    cout << "Digite os números de dois dígitos e aperte enter depois de cada um deles." << endl;
+    cout << "Digite 'e' para sair do programa." << endl;
+
+
+        while(x >= 0){
+
+        cin >> x;
+
+        if(x >=0 && x<25){
+            a++;
+        }else if(x >=25 && x<50){
+            b++;
+        }else if(x >=50 && x<75){
+            c++;
+        }else if(x >=75 && x<100){
+            d++;
+        }else{
+            e++;
+        }
+
+    }
+
+    A = a/(a+b+c+d+e);
+    cout << A << endl;
+
+    return 0;
+
+}
