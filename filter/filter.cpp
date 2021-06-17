448c8cdf4d4100c323f5e690c248cdf03072719e
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-06-17 12:20:17 -0300

    Update filter.cpp

--- filter/filter.cpp
+++ filter/filter.cpp
@@ -13,10 +13,12 @@ int main(){
     //Permite usar acentos
     setlocale(LC_ALL,"");
 
+    //Entrando com os vetores e variáveis
     int vetor1[10] = {-2, -8, 6, 7, -3, 10, 1, 0, -3 ,7};
     int vetor2[10];
     int j = 0;
 
+    //Pegando os numeros não negativos e não nulos e colocando no inicio do segundo vetor
     for(int i = 0; i <= 10; i++){      
       if(vetor1[i] > 0){
         vetor2[j] = vetor1[i];
@@ -24,10 +26,10 @@ int main(){
       }
     }
 
+    //Imprimindo o segundo vetor
     for(int i = 0; i <= 10; i++){
       cout << vetor2[i] << " ";
     }
 
-    //system("pause");
     return 0;
 }
