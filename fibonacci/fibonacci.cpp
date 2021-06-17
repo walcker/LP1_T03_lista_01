1b93fa7388be44cecef124b529268c438427c70d
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-06-17 12:17:48 -0300

    Update fibonacci.cpp

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -9,32 +9,21 @@
 
 using namespace std;
 
-/*void fib_below_n (int n) {
-
-    int x =0, aux;
-    int i = 1;
-    while (i < n){
-      aux = i + x ;
-      x = i;
-      i = aux;
-
-      cout << i << endl;
-
-    }
-}*/
-
 int main(){
     //Permite usar acentos
     setlocale(LC_ALL,"");
 
+    //Declarando variáveis
     int n;
     int x =0, aux;
     int i;
 
+    //Requisitando valor ao usuário
     cout << "entre com o valor." << endl;
     cin >> n; 
     cout << 1 << endl;      
 
+    //Contagem da série até i número escolhido
     for(i = 1; i < (n - 3);){
       aux = i + x ;
       x = i;
@@ -44,9 +33,6 @@ int main(){
 
     };
 
-    //int fib_below_n(n);
-
-    //system("pause");
     return 0;
 }
 
