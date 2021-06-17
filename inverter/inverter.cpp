687d58406f16e0e5d114a241cde152112f709f41
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-06-17 12:25:23 -0300

    Update inverter.cpp

--- inverter/inverter.cpp
+++ inverter/inverter.cpp
@@ -13,19 +13,21 @@ int main(){
     //Permite usar acentos
     setlocale(LC_ALL,"");
 
+    //Declaração de variáveis
     int vetor1[5] = {1, 2, 3, 4, 5};
     int vetor2[5];
     int j = 4;
 
+    //Inverter o vetor 1 no vetor 2
     for(int i = 0; i < 5; i++){
       vetor2[j] = vetor1[i];
       j--;
     }
 
+    //Imprimir vetor invertido na tela
     for(int i = 0; i <5 ; i++){
       cout << vetor2[i] << endl;
     }
 
-    //system("pause");
     return 0;
 }
